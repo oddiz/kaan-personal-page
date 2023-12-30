c488f62f74f49bcb69f97a45e3ee4c1089b47c44
oddiz <kaansarkaya@gmail.com>
2023-12-30 17:58:10 +0300

    codemod nextjsj

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+export default function NextIndexWrapper() {
+    return (
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
