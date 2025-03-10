e8aa6640977a082620dfa1f9edb458d5e30cdc03
oddiz <kaansarkaya@gmail.com>
2025-03-10 16:01:53 +0300

    updated projects

--- src/components/ProjectBanner.js
+++ src/components/ProjectBanner.js
@@ -1,191 +1,159 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const BORDER_COLOR = "#03525F";
 const BORDER_THICKNESS = "1px";
 
 const ProjectBannerContainer = styled.div`
-    width: 280px;
-    
-    display: flex;
-    flex-direction: column;
+  width: 280px;
 
-    margin: 10px 20px;
+  display: flex;
+  flex-direction: column;
 
-    transition: all 0.3s ease-in-out;
-        
-`
+  margin: 10px 20px;
+
+  transition: all 0.3s ease-in-out;
+`;
 
 const ProjectHeader = styled.div`
+  height: 80px;
+  width: 100%;
 
-    height: 80px;
-    width: 100%;
+  box-sizing: border-box;
 
-    box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
 
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
+  background-size: 100px;
+  background-repeat: no-repeat;
+  background: ${(props) => props.backgroundColor || "black"};
+  background-position: center;
 
-    background-size: 100px;
-    background-repeat: no-repeat; 
-    background: ${props => props.backgroundColor || "black"};
-    background-position: center;
-
-    border-top: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
-    border-right: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
-    border-left: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
-`
+  border-top: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
+  border-right: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
+  border-left: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
+`;
 
 const ProjectHeaderImage = styled.img`
-
-    height: 79px;
-
-`
+  height: 79px;
+`;
 
 const ProjectBody = styled.div`
-    width: 100%;
-    height: 350px;
-    padding: 25px 20px;
-     
-    box-sizing: border-box;
-    
+  width: 100%;
+  height: 350px;
+  padding: 25px 20px;
 
-    font-family: 'Ubuntu', sans-serif;
-    color: white;
-    
-    display: flex;
-    flex-direction: column;
+  box-sizing: border-box;
 
+  font-family: "Ubuntu", sans-serif;
+  color: white;
 
-    @media (max-height: 620px),
-           (max-width: 600px) {
-        height: 150px;
-        padding: 10px 20px;
+  display: flex;
+  flex-direction: column;
 
-    }
+  @media (max-height: 620px), (max-width: 600px) {
+    height: 150px;
+    padding: 10px 20px;
+  }
 
-    border-right: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
-    border-left: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
-    
-    transition: all 0.3s ease-in-out;
+  border-right: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
+  border-left: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
 
-`
+  transition: all 0.3s ease-in-out;
+`;
 
 const ProjectTitle = styled.h1`
-    font-size: 1.5em;
-    font-weight: 500;
-    margin-top: 0px;
-    
-    @media (max-height: 620px),
-           (max-width: 600px) {
-        margin: 15px 0px;
-    }
+  font-size: 1.5em;
+  font-weight: 500;
+  margin-top: 0px;
 
-`
+  @media (max-height: 620px), (max-width: 600px) {
+    margin: 15px 0px;
+  }
+`;
 
 const ProjectDescription = styled.p`
+  font-size: 0.9em;
+  font-weight: 300;
+  margin-bottom: 40px;
 
-    font-size: 0.9em;
-    font-weight: 300;
-    margin-bottom: 40px;
-
+  text-align: center;
+  white-space: pre-line;
+  font-family: "Source Code Pro";
+  @media (max-height: 620px), (max-width: 600px) {
     text-align: center;
-    white-space: pre-line;
-    font-family: "Source Code Pro";
-    @media (max-height: 620px),
-           (max-width: 600px) {
-            text-align: center;
-
-
-    }
-` 
+  }
+`;
 
 const ProjectFooter = styled.div`
-    box-sizing: border-box;
-    height: 60px;
-    width: 100%;
+  box-sizing: border-box;
+  height: 60px;
+  width: 100%;
 
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 
-    border-right: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
-    border-left: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
-    border-bottom: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
-`
+  border-right: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
+  border-left: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
+  border-bottom: ${BORDER_THICKNESS} solid ${BORDER_COLOR};
+`;
 
 const ProjectButton = styled.a`
+  background: #225e5a;
+  height: 40px;
+  width: 140px;
+  margin-bottom: 15px;
 
-    background: #225E5A;
-    height: 40px;
-    width: 140px;
-    margin-bottom: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  color: white;
+  border-radius: 5px;
 
-    color: white;
-    border-radius: 5px;
+  &:hover {
+    background: #2b8d86;
+  }
+`;
 
-    &:hover {
-        background: #2B8D86;
-    }
-`
+function ProjectBanner(props) {
+  let description = props.description;
 
-function ProjectBanner (props) {
-    
+  if (props.miniMode) {
+    //split string from new lines
+    description = description.split("\n")[0];
+  }
 
-    let description = props.description
+  return (
+    <ProjectBannerContainer>
+      <ProjectHeader backgroundColor={props.backgroundColor}>
+        <ProjectHeaderImage src={props.backgroundImage} />
+      </ProjectHeader>
 
-    if (props.miniMode) {
-        //split string from new lines
-        description = description.split("\n")[0]
+      <ProjectBody>
+        <ProjectTitle>{props.title}</ProjectTitle>
+        <ProjectDescription>{description}</ProjectDescription>
+      </ProjectBody>
 
-    }
-
-    return (
-        <ProjectBannerContainer>
-            <ProjectHeader 
-                backgroundColor={props.backgroundColor}
-            >
-                <ProjectHeaderImage 
-                    src= {props.backgroundImage}
-                    
-                />
-            </ProjectHeader>
-
-            <ProjectBody>
-                <ProjectTitle>
-                    {props.title}
-                </ProjectTitle>
-                <ProjectDescription>
-                    {description}
-
-                </ProjectDescription>
-
-
-            </ProjectBody>
-
-            <ProjectFooter>
-                <ProjectButton 
-                    href={props.link}
-                    target="_blank"
-                >
-                    Website
-                </ProjectButton>
-            </ProjectFooter>
-        </ProjectBannerContainer>
-    )
+      <ProjectFooter>
+        {props.disabled ?? (
+          <ProjectButton href={props.link} target="_blank">
+            Website
+          </ProjectButton>
+        )}
+      </ProjectFooter>
+    </ProjectBannerContainer>
+  );
 }
 
-export default ProjectBanner
+export default ProjectBanner;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,83 +1,83 @@
-import styled from 'styled-components';
-import FadeIn from 'react-fade-in/lib/FadeIn';
-import ProjectBanner from '../components/ProjectBanner';
-import useWindowSize from '../components/useWindowSize';
-
-
+import styled from "styled-components";
+import FadeIn from "react-fade-in/lib/FadeIn";
+import ProjectBanner from "../components/ProjectBanner";
+import useWindowSize from "../components/useWindowSize";
 
 const Content = styled.div`
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 
-    margin-top: 20px;
+  margin-top: 20px;
 
-    text-align: center;
+  text-align: center;
 
-    font-family: 'Ubuntu', sans-serif;
-    color: white;
+  font-family: "Ubuntu", sans-serif;
+  color: white;
 
-    
-    
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-    @media (max-height: 800px) {
-       
-    }
+  @media (max-height: 800px) {
+  }
 
-    transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
 
-    z-index: 10;
-`
+  z-index: 10;
+`;
 
 const FadeInFlexed = styled(FadeIn)`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
-    @media (max-width: 600px) {
-
-        flex-direction: column;
-    }
-`
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
 
 function Projects() {
+  const { width, height } = useWindowSize();
 
-    const { width, height } = useWindowSize();
-     
-    let miniMode = false 
+  let miniMode = false;
 
-    if (height < 620 || width < 600) {
-        miniMode =true
-    }
+  if (height < 620 || width < 600) {
+    miniMode = true;
+  }
 
-    return (
-        <Content>
-            <FadeInFlexed>
-                <ProjectBanner 
-                    title="Ecofood"
-                    description={`Skillpoint calculator based on what you eat for Eco game.\n\nThis is the first app that I've made after completing an Udemy course for JS and web developement.\n\nFeatures a simple vanilla js frontend + backend for storing highscores.`}
-                    link="https://ecofood.app/"
-                    backgroundImage="img/ecodiet_logo.png"
-                    backgroundColor="#5A5C84"
-                    miniMode = {miniMode}
-                />
-                <ProjectBanner 
-                    title="Botdiz"
-                    description={`A music bot for discord.\n\nFeatures a nice web interface to control the music player and even import spotify playlists into it.\n\nMy first React webpage and proper full stack project including MongoDB, external and internal auth, websockets.`}
-                    link="https://botdiz.kaansarkaya.com/"
-                    backgroundImage="img/botdiz_logo.png"
-                    backgroundColor="linear-gradient(132deg, rgba(31,29,40,1) 44%, rgba(44,44,62,1) 60%);"
-                    miniMode = {miniMode}
-
-                />
-                
-
-            </FadeInFlexed>
-        </Content>
-    )
+  return (
+    <Content>
+      <FadeInFlexed>
+        <ProjectBanner
+          title="Ecofood"
+          description={`Skillpoint calculator based on what you eat for Eco game.\n\nThis is the first app that I've made after completing an Udemy course for JS and web developement.\n\nFeatures a simple vanilla js frontend + backend for storing highscores.`}
+          link="https://ecofood.app/"
+          backgroundImage="img/ecodiet_logo.png"
+          backgroundColor="#5A5C84"
+          miniMode={miniMode}
+          disabled={true}
+        />
+        <ProjectBanner
+          title="Ecofood v2"
+          description={`Version 2 of my first project 6 years later. Bootstrapped from Next.js\n\nThis time I've used React, shadcn, tailwind for the frontend.\n\nFeatures a more modern design and a more responsive layout with various new features.\n\n `}
+          link="https://ecofood.app/"
+          backgroundImage="img/ecodiet_logo.png"
+          backgroundColor="#5A5C84"
+          miniMode={miniMode}
+        />
+        <ProjectBanner
+          title="Botdiz"
+          description={`A music bot for discord.\n\nFeatures a nice web interface to control the music player and even import spotify playlists into it.\n\nMy first React webpage and proper full stack project including MongoDB, external and internal auth, websockets.`}
+          link="https://botdiz.kaansarkaya.com/"
+          backgroundImage="img/botdiz_logo.png"
+          backgroundColor="linear-gradient(132deg, rgba(31,29,40,1) 44%, rgba(44,44,62,1) 60%);"
+          miniMode={miniMode}
+          disabled={true}
+        />
+      </FadeInFlexed>
+    </Content>
+  );
 }
 
 export default Projects;
